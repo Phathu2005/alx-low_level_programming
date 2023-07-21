e41e2da4e146f36649657ccce416d79811a1c40b
Phathu2005 <lithudzhphathutshedzo@gmail.com>
2023-07-21 12:16:34 -0700

    To hell with circumstances

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: array
+ * @size: size of elements in the array
+ * @cmp: pointer to func of one of the 3 in main
+ *
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+
+		return (-1);
+	for (j = 0; j < size; j++)
+	{
+		if (cmp(array[j]))
+
+			return (j);
+	}
+	return (-1);
+}
