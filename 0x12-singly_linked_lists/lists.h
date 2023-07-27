bf1d39f416771cb897bba9f0924e754724862bcd
Phathu <lithudzhaphathutshedzo@gmail.com>
2023-07-27 12:15:00 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
+ *
  * Description: singly linked list node structure
- * for Holberton project
+ * for project
  */
 typedef struct list_s
 {
@@ -23,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
